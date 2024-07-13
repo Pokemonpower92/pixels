301271a490953a7146eb107bfcc6f3a41f04d03f
jcarmac3 <pokemonpower92@gmail.com>
2024-07-13 10:29:03 -0600

    Stubs.

--- internal/jobhandler/isjobhandler_test.go
+++ internal/jobhandler/isjobhandler_test.go
@@ -1,69 +1,134 @@
 package jobhandler
 
 import (
-	"image/color"
+	"errors"
 	"log"
-	"os"
+	"strconv"
 	"testing"
 
 	"github.com/pokemonpower92/collagegenerator/internal/domain"
 	"github.com/pokemonpower92/collagegenerator/internal/job"
+	"github.com/pokemonpower92/collagegenerator/internal/stubs"
 )
-
-type mockISRepo struct{}
-
-func (m *mockISRepo) Create(imageSet *domain.ImageSet) error {
-	return nil
-}
-
-func (m *mockISRepo) Get(id int) (*domain.ImageSet, bool) {
-	return nil, false
-}
-
-func (m *mockISRepo) Update(id int, imageSet *domain.ImageSet) (*domain.ImageSet, error) {
-	return nil, nil
-}
-
-func (m *mockISRepo) Delete(id int) (*domain.ImageSet, error) {
-	return nil, nil
-}
-
-type mockGenerator struct{}
-
-func (m *mockGenerator) Generate(job *job.Job) (*domain.ImageSet, error) {
-	return &domain.ImageSet{
-		ID:          1,
-		Name:        "Test",
-		Description: "Test",
-		AverageColors: []*color.RGBA{
-			{R: 255, G: 255, B: 255, A: 255},
-		},
-	}, nil
-}
 
 func TestISJobHandler(t *testing.T) {
 	tests := []struct {
-		name string
+		name         string
+		job          *job.Job
+		getFunc      func(id int) (*domain.ImageSet, bool)
+		createFunc   func(obj *domain.ImageSet) error
+		generateFunc func(job *job.Job) (*domain.ImageSet, error)
+		expected     error
 	}{
 		{
-			name: "TestISJobHandler",
+			name: "invalid imageset id",
+			job: &job.Job{
+				ImagesetID:  "bad id",
+				BucketName:  "test bucket",
+				Description: "test description",
+			},
+			getFunc: func(id int) (*domain.ImageSet, bool) {
+				return nil, false
+			},
+			createFunc: func(obj *domain.ImageSet) error {
+				return nil
+			},
+			generateFunc: func(job *job.Job) (*domain.ImageSet, error) {
+				return nil, nil
+			},
+			expected: &strconv.NumError{Func: "Atoi", Num: "bad id", Err: strconv.ErrSyntax},
+		},
+		{
+			name: "imageset found",
+			job: &job.Job{
+				ImagesetID:  "1",
+				BucketName:  "test bucket",
+				Description: "test description",
+			},
+			getFunc: func(id int) (*domain.ImageSet, bool) {
+				return &domain.ImageSet{}, true
+			},
+			createFunc: func(obj *domain.ImageSet) error {
+				return nil
+			},
+			generateFunc: func(job *job.Job) (*domain.ImageSet, error) {
+				return &domain.ImageSet{}, nil
+			},
+			expected: errors.New(""),
+		},
+		{
+			name: "imageset not found",
+			job: &job.Job{
+				ImagesetID:  "1",
+				BucketName:  "test bucket",
+				Description: "test description",
+			},
+			getFunc: func(id int) (*domain.ImageSet, bool) {
+				return nil, false
+			},
+			createFunc: func(obj *domain.ImageSet) error {
+				return nil
+			},
+			generateFunc: func(job *job.Job) (*domain.ImageSet, error) {
+				return &domain.ImageSet{}, nil
+			},
+			expected: errors.New(""),
+		},
+		{
+			name: "failed to generate imageset",
+			job: &job.Job{
+				ImagesetID:  "1",
+				BucketName:  "test bucket",
+				Description: "test description",
+			},
+			getFunc: func(id int) (*domain.ImageSet, bool) {
+				return nil, false
+			},
+			createFunc: func(obj *domain.ImageSet) error {
+				return nil
+			},
+			generateFunc: func(job *job.Job) (*domain.ImageSet, error) {
+				return nil, errors.New("failed to generate imageset")
+			},
+			expected: errors.New("failed to generate imageset"),
+		},
+		{
+			name: "failed to create imageset",
+			job: &job.Job{
+				ImagesetID:  "1",
+				BucketName:  "test bucket",
+				Description: "test description",
+			},
+			getFunc: func(id int) (*domain.ImageSet, bool) {
+				return nil, false
+			},
+			createFunc: func(obj *domain.ImageSet) error {
+				return errors.New("failed to create imageset")
+			},
+			generateFunc: func(job *job.Job) (*domain.ImageSet, error) {
+				return &domain.ImageSet{}, nil
+			},
+			expected: errors.New("failed to create imageset"),
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			logger := log.New(os.Stdout, "test ", log.LstdFlags)
-			repo := &mockISRepo{}
-			generator := &mockGenerator{}
-			isjh := NewISJobHandler(logger, repo, generator)
-
-			job := &job.Job{
-				ImagesetID: "1",
+			repo := &stubs.RepositoryStub[domain.ImageSet]{
+				GetFunc:    tt.getFunc,
+				CreateFunc: tt.createFunc,
 			}
 
-			err := isjh.HandleJob(job)
-			if err != nil {
-				t.Errorf("Failed to handle job: %s", err)
+			gen := &stubs.GeneratorStub{
+				GenerateFunc: tt.generateFunc,
+			}
+
+			logger := log.New(log.Writer(), "test: ", log.Flags())
+			handler := NewISJobHandler(logger, repo, gen)
+
+			err := handler.HandleJob(tt.job)
+			if errors.Is(err, tt.expected) {
+				t.Errorf("expected error %v, got %v", tt.expected, err)
 			}
 		})
 	}
