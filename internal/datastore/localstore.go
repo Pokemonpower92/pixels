d47f8a11039ec8a6dfd6297d06783c7480dca8aa
pooch <pokemonpower92@gmail.com>
2024-10-06 09:17:55 -0600

    LocalStore implemented

--- internal/datastore/localstore.go
+++ internal/datastore/localstore.go
@@ -1,7 +1,10 @@
 package datastore
 
 import (
+	"fmt"
 	"image"
+	"image/draw"
+    _ "image/png"
 	"log"
 	"os"
 )
@@ -18,13 +21,42 @@ func NewLocalStore(directory string) *LocalStore {
 	}
 }
 
+func imageToRGBA(src image.Image) *image.RGBA {
+    if dst, ok := src.(*image.RGBA); ok {
+        return dst
+    }
+    b := src.Bounds()
+    dst := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
+    draw.Draw(dst, dst.Bounds(), src, b.Min, draw.Src)
+    return dst
+}
+
 func (s *LocalStore) GetImages() ([]*image.RGBA, error) {
 	s.logger.Printf("Reading images from directory: %s", s.Directory)
-	f, err := os.Open(s.Directory)
+	d, err := os.ReadDir(s.Directory)
 	if err != nil {
 		s.logger.Printf("Failed to open directory: %s", err)
 		return nil, err
 	}
-	defer f.Close()
-
+    imageSet := make([]*image.RGBA, 0)
+    for i, item := range d {
+        path := fmt.Sprintf("%s/%s", s.Directory, item.Name())
+        s.logger.Printf("Decoding file %d: %s", i, path)
+        f, err := os.Open(path)
+        if err != nil {
+            s.logger.Printf("Failed to open file: %s", item.Name())
+            return nil, err
+        }
+        defer f.Close()
+        im, _, err := image.Decode(f)
+        if err != nil {
+            s.logger.Printf("Failed to decode image: %s", err)
+            return nil, err
+        }
+        b := im.Bounds()
+        rgba := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
+        draw.Draw(rgba, rgba.Bounds(), im, b.Min, draw.Src)
+        imageSet = append(imageSet, rgba)
+    }
+    return imageSet, nil
 }
