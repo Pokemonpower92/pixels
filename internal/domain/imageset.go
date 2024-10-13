ecc67a751e04f36dac12b674c4c4b8af639f173a
mimikyu <pokemonpower92@gmail.com>
2024-10-13 09:53:53 -0600

    cleaned up repository

--- internal/domain/imageset.go
+++ internal/domain/imageset.go
@@ -2,9 +2,14 @@ package domain
 
 import "image/color"
 
+type AverageColor struct {
+	FileName string     `json:"file_name"`
+	Color    color.RGBA `json:"color"`
+}
+
 type ImageSet struct {
-	ID            int          `json:"id"`
-	Name          string       `json:"name"`
-	Description   string       `json:"description"`
-	AverageColors []color.RGBA `json:"average_colors"`
+	ID            int             `json:"id"`
+	Name          string          `json:"name"`
+	Description   string          `json:"description"`
+	AverageColors []*AverageColor `json:"average_colors"`
 }
