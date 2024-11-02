aac687d22c3a86bccc4f22641673f01e0a2275b5
pooch <pokemonpower92@gmail.com>
2024-11-02 10:14:32 -0600

    work

--- internal/service/collage.go
+++ internal/service/collage.go
@@ -8,4 +8,18 @@ import (
 
 func CreateCollage(collage *sqlc.Collage) {
 	fmt.Printf("Creating collage: %+v\n", collage)
+	// Find all of the images and where they will go
+	// Do this concurrently for each section of the
+	// target image.
+	// Store the results in the collage_images table.
+
+	// Construct the collage.
+}
+
+func findAverageColorForSection() {
+	fmt.Printf("Finding average color for section.")
+}
+
+func determineImagePlacements() {
+	fmt.Printf("Finding average color for section.")
 }
