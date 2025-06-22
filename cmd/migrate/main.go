856ac7b9a2d50460a8cab6ecc132868f7a24af55
jcarmac3 <pokemonpower92@gmail.com>
2025-06-22 09:22:05 -0600

    chore: fix migration

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -7,14 +7,12 @@ import (
 	"github.com/jackc/pgx/v5"
 	"github.com/pokemonpower92/pixels/config"
 	"github.com/pokemonpower92/pixels/internal/database"
-	"github.com/pokemonpower92/pixels/internal/repository"
 )
 
 func main() {
 	time.Sleep(1000 * time.Millisecond)
 	log.Printf("Migrating database...")
-	postgresConfig := config.NewPostgresConfig()
-	connString := repository.GetConnectionString(postgresConfig)
+	connString := config.ConnString()
 	config, err := pgx.ParseConfig(connString)
 	if err != nil {
 		panic(err)
